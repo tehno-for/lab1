47910049669f1ac90842f971af8094010c0651cd
tehno-for <keoroot@gmail.com>
2022-05-01 01:14:28 +0400

    install ansible

--- main.tf
+++ main.tf
@@ -12,8 +12,10 @@ provider "aws" {
 
 resource "aws_instance" "jira_app" {
   count = 3
-  ami = "ami-042ad9eec03638628"
+  ami = "ami-0d51579f02ac97d77"
   instance_type = "t2.micro"
+  # os            = "Ubuntu, 18.04 LTS"
+	security_groups = ["launch-wizard-7"]
   key_name   = "fow_work"
   tags = {
     Name = "jira_app-${count.index}"
@@ -26,12 +28,21 @@ output "app_privat_ip" {
 
 resource "aws_instance" "db_server" {
   count = 1
-  ami = "ami-042ad9eec03638628"
+  ami = "ami-0d51579f02ac97d77"
   instance_type = "t2.micro"
+  # os            = "Ubuntu, 18.04 LTS"
+  security_groups = ["launch-wizard-7"]
   key_name   = "fow_work"
   tags = {
     Name = "db_server-${count.index}"
   }
+    user_data = <<-EOF
+          #!/bin/bash 
+          sudo apt -y update
+          apt install -y software-properties-common
+          apt-add-repository --yes --update ppa:ansible/ansible
+          apt install -y ansible
+          EOF
 }
 
 output "db_privat_ip" {
